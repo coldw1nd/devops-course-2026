901007179f3355692ebf6aec3a2e08aceeb70bb3
Станислав Шувалов <shuvalov.s.d@edu.mirea.ru>
2026-09-22 20:43:19 +0300

    docs: add calculator README

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 # Calculator app
 def add(a, b):
-return a + b
+    return a + b
 # TODO: add more functions
-def subtract(a, b):
 
-    return a - b # fixed
+def subtract(a, b):
+    return a - b  # fixed
